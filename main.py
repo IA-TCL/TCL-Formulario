1afb497f00f1e6128c588f4e23765120ea4e6449
Yeiler <ia@tclasesores.com>
2026-06-05 08:58:03 -0500

    Mas peque

--- main.py
+++ main.py
@@ -463,7 +463,7 @@ async def gracias():
             text-align: center;
         }
         .logo-img {
-            width: 270px;
+            width: 265px;
             height: auto;
             display: block;
             margin-bottom: 28px;
